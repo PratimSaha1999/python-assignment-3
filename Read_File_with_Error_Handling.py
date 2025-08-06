f1b170508b5454f2a4dbfc3bfa561e1b8f49f710
PRATIM SAHA <sahapratim1999@gmail.com>
2025-08-06 07:22:28 +0530

    Delete Read_File_with_Error_Handling.py

--- Read_File_with_Error_Handling.py
+++
@@ -1,9 +0,0 @@
-# Step 1: Try opening and reading the file
-try:
-    with open("sample.txt", "r") as file:
-        print("Contents of 'sample.txt':")
-        for line in file:
-            print(line.strip())
-
-except FileNotFoundError:
-    print("Error: The file 'sample.txt' does not exist.")
